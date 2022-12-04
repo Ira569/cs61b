eee5af5b05e265987ba33033718d2ab25c2313b3
Ira569 <1343867315@qq.com>
2022-12-04 22:03:56 +0800

    finish lab3

--- lab3/Flik/FlikTest.java
+++ lab3/Flik/FlikTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class FlikTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void isSameNumberTest(){
+        boolean output = Flik.isSameNumber(1,2);
+        assertTrue(!output);
+        output =Flik.isSameNumber(3,3);
+        assertTrue("3==3" ,output);
+        output =Flik.isSameNumber(128,128);
+        assertTrue("128==128",output);
+
+
+    }
 }
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -5,6 +5,42 @@ import org.junit.Test;
 public class IntListTest {
 
     /**
+     * Your test should test at least the following three situations:
+     * That the function returns a reversed list.
+     * That the function is destructive, i.e. when it is done running, the list pointed to by A has been tampered with. You can use assertNotEquals. This is sort of a silly test.
+     * That the method handles a null input properly.
+     */
+    @Test(timeout = 1000)
+    public  void  reverseTest() {
+        //case 1:
+        //input = 0-> 1-> 2-> 3->null
+        //output should be 3->2->1->0->null
+        IntList input1 = IntList.of(0, 1, 2, 3);
+        IntList output1 = IntList.reverse(input1);
+        IntList expected1 = IntList.of(3, 2, 1, 0);
+        assertEquals(expected1,output1);
+
+        //case 2
+        //after reverse,the content in input should be changed.
+        IntList input2=IntList.of(4, 5, 6, 7);
+        IntList.reverse(input2);
+        IntList expected2=IntList.of(4, 5, 6, 7);
+        assertNotEquals(expected2,input2);
+
+        //case 3
+        //the reverse of null should be handled.
+        IntList input3 =null;
+        IntList output3=IntList.reverse(input3);
+        IntList expected3 = null;
+        assertEquals(expected3,output3);
+
+
+
+
+
+
+    }
+    /**
      * Example test that verifies correctness of the IntList.of static
      * method. The main point of this is to convince you that
      * assertEquals knows how to handle IntLists just fine.
