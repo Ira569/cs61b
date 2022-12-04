140b19c9f32c4ff1e52405991e47fb1e7328aba3
Ira569 <1343867315@qq.com>
2022-12-04 22:15:07 +0800

    change code style

--- lab3/Flik/FlikTest.java
+++ lab3/Flik/FlikTest.java
@@ -3,14 +3,12 @@ import org.junit.Test;
 
 public class FlikTest {
     @Test
-    public void isSameNumberTest(){
-        boolean output = Flik.isSameNumber(1,2);
+    public void isSameNumberTest() {
+        boolean output = Flik.isSameNumber(1, 2);
         assertTrue(!output);
-        output =Flik.isSameNumber(3,3);
-        assertTrue("3==3" ,output);
-        output =Flik.isSameNumber(128,128);
-        assertTrue("128==128",output);
-
-
+        output = Flik.isSameNumber(3, 3);
+        assertTrue("3==3", output);
+        output = Flik.isSameNumber(128, 128);
+        assertTrue("128==128", output);
     }
 }
--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,6 +1,7 @@
 public class HorribleSteve {
     public static void main(String [] args) {
-        int i = 0;int j=0;
+        int i = 0;
+        int j = 0;
         for (; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 break; // break exits the for loop!
