137349933b53139c4c4767bd5cd40320d3507fc9
Ira569 <1343867315@qq.com>
2022-12-15 18:19:19 +0800

    done

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,6 @@
 public class OffByOne  implements CharacterComparator{
     @Override
     public  boolean equalChars(char x, char y) {
-        if((x >= 'a' && x <= 'z')||(x >= 'A' && x <= 'Z')   && ((y >= 'a' && y <= 'z')||(y >= 'A' && y <= 'Z')))
-            return  x == (y + 1) || (x + 1) == y;
+        return  x == (y + 1) || (x + 1) == y;
     }
 }
